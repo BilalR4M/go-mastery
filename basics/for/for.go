759ef7846b82f25dbe688e21a6f96f14317a74c5
BilalR4M <bilalawshid@gmail.com>
2026-01-22 00:01:05 +0530

    Add additional for loop examples and demonstrate range usage

--- basics/for/for.go
+++ basics/for/for.go
@@ -14,4 +14,24 @@ func main() {
 		time.Sleep(1 * time.Second)
 	}
 
+	for j := 1; j <= 3; j++ {
+		fmt.Println(j)
+	}
+
+	for i := range 3 {
+		fmt.Println(i)
+	}
+
+	for {
+		fmt.Println("loop")
+		break
+	}
+
+	for n := range 6 {
+		if n%2 == 0 {
+			continue
+		}
+		fmt.Println(n)
+	}
+
 }
