87e4c2463982ffdc5a7d598dcfaa98381e0302aa
BilalR4M <bilalawshid@gmail.com>
2026-01-21 21:35:59 +0530

    Refactor error handling to use log.Fatal for improved logging

--- basics/hello/hello.go
+++ basics/hello/hello.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"bilal.com/greetings"
 )
 
 func main() {
+	// Set properties of the predifined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	// Get a greeting message and print it.
 	message, err := greetings.Hello("")
+
+	// If an error was returned, print it to the console
+	// exit the program
+
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		log.Fatal(err)
 	}
+
 	fmt.Println(message)
 }
